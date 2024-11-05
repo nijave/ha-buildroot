19027bc79657b11617afb304648f52ea160cf445
Jan Čermák <sairon@sairon.cz>
2024-11-05 13:03:36 +0100

    package/rpi-firmware: bump to version 1.20241008 for kernel 6.6.51

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 3590de0c181d433af368a95f15bc480bdaff8b47
+RPI_FIRMWARE_VERSION = 7bbb5f80d20a2335066a8781459c9f33e5eebc64
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
